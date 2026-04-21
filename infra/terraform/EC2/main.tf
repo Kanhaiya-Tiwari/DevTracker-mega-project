dc12206ca760c64c33e4066ffbc5871822d6ae6a
Kanhaiya Tiwari <kanhaiyatiwari506@gmail.com>
2026-04-21 21:39:54 +0530

    Change services to NodePort for direct EC2 access

--- infra/terraform/EC2/main.tf
+++ infra/terraform/EC2/main.tf
@@ -98,6 +98,20 @@ resource "aws_security_group" "sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 30900
+    to_port     = 30900
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 30300
+    to_port     = 30300
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
