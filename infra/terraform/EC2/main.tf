79f806ce0b6f9be81da36943a5eafbf3ddf259c4
Kanhaiya Tiwari <kanhaiyatiwari506@gmail.com>
2026-04-21 21:40:59 +0530

    Add ArgoCD NodePort 30443 for direct EC2 access

--- infra/terraform/EC2/main.tf
+++ infra/terraform/EC2/main.tf
@@ -112,6 +112,13 @@ resource "aws_security_group" "sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 30443
+    to_port     = 30443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
